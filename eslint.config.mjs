909a834a5220af469df50630ab522214eb33c1cc
Rajvardhan <rajvardhan@agrim.app>
2026-01-28 21:13:16 +0530

    Disable no-img-element ESLint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,29 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals"),
+//   {
+//     ignores: [
+//       "node_modules/**",
+//       ".next/**",
+//       "out/**",
+//       "build/**",
+//       "next-env.d.ts",
+//     ],
+//   },
+// ];
+
+// export default eslintConfig;
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -10,7 +36,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Next.js core rules
   ...compat.extends("next/core-web-vitals"),
+
+  // Custom overrides
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+    },
+  },
+
+  // Ignore files
   {
     ignores: [
       "node_modules/**",
